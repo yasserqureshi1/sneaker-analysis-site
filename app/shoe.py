34979a5b3dbd751cccc20d2961935337edd37edd
yasser <yasser.q@hotmail.com>
2020-12-03 01:12:50 +0000

    Adding shoe.html

--- app/shoe.py
+++ app/shoe.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask import render_template
-
+from app import StockXscraper
 
 shoe = Blueprint("shoe", __name__)
 
 
-@shoe.route('/shoe')
-def shoe_page():
-    return render_template('shoe.html')
+@shoe.route('/<item>')
+def product_item(item):
+    data = StockXscraper.getProductDetails(item)
+    return render_template('shoe.html', data=data)
+
