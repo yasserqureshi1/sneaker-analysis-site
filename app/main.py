1f7dc89f20ffbe5018cb5b716d6cb331f0c8ce7f
yasser <yasser.q@hotmail.com>
2020-12-03 01:11:59 +0000

    Adding selection.html

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, url_for
 from flask import render_template
+from app import StockXscraper
 
 
 home = Blueprint("home", __name__)
@@ -8,3 +9,11 @@ home = Blueprint("home", __name__)
 @home.route('/')
 def home_page():
     return render_template('home.html')
+
+
+@home.route('/', methods=['POST'])
+def search():
+    shoe_name = request.form['sneaker']
+    names = StockXscraper.findItem(shoe_name)
+    return render_template('selection.html', results=names)
+
