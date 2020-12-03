eaed4a8b2998e13e96209ef010f966617ec576d9
yasser <yasser.q@hotmail.com>
2020-12-03 01:11:28 +0000

    Registering 'shoe' blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from . import main, shoe
 
+
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
 
     app.register_blueprint(main.home)
-    #app.register_blueprint(shoe.sneaker)
+    app.register_blueprint(shoe.shoe)
     return app
