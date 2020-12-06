11310783a4d8cb864c0f89c12fa749d8124e2aa9
Yasser Qureshi <yasser.q@hotmail.com>
2020-12-06 22:16:25 +0000

    Clean up and update

--- app/twitterScraper.py
+++ app/twitterScraper.py
@@ -1,15 +1,12 @@
 # Need to fix and clean up
-# - Twitter streamer
 # - Last 100 Tweets
 # - return text and sentiment
 
 import tweepy
 from textblob import TextBlob
-import preprocessor as p
 import re
-import matplotlib.pyplot as plt
-import numpy as np
 import config
+import string
 
 auth = tweepy.OAuthHandler(config.api_key, config.api_secret_key)
 auth.set_access_token(config.access_token, config.access_secret_token)
@@ -19,34 +16,73 @@ api = tweepy.API(auth)
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        self.clean_tweet(status.text)
-        self.sentiment(status.text)
+        """
+        Returns the full text of a searched tweet via the Twitter Streamer
+        """
+        if hasattr(status, "retweeted_status"):
+            try:
+                text = status.retweeted_status.extended_tweet["full_text"]
+            except AttributeError:
+                text = status.retweeted_status.text
 
-    def clean_tweet(self, tweet):
-        tweet = re.sub(r'http\S+', '', tweet)
-        p.clean(tweet)
-        strip = [':', '.', '-', '+', '[', ']', '{', '}', '!', '@', '^', '%', '&', '*', '(', ')', '...']
-        for i in strip:
-            tweet = tweet.replace(i, '')
-        print(tweet)
+        else:
+            try:
+                text = status.extended_tweet["full_text"]
+            except AttributeError:
+                text = status.text
 
-    def sentiment(self, tweet):
-        analysis = TextBlob(tweet)
-        print(analysis.sentiment[0])
-        return analysis.sentiment[0]
+        self.get_details(status)
+
+        return text
+
+    def get_details(self, status):
+        """
+        Returns the tweet author, handle, date/time of the tweet (for display purposes)
+        """
+        return status.user.name, status.user.screen_name, status.user.location, status.created_at
+
+
+def preprocess_tweet(tweet):
+    """
+    Prepares the text in a tweet for sentiment analysis. Returns tokenized and cleaned words
+    """
+
+    # Remove Links
+    text = re.sub(r'http\S+', '', tweet)
+
+    # Remove Emojis
+    regrex_pattern = re.compile(pattern="["
+                                        u"\U0001F600-\U0001F64F"  # emoticons
+                                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                        "]+", flags=re.UNICODE)
+    text = regrex_pattern.sub(r'', text)
+
+    # Remove punctuation and numbers
+    text = "".join([char for char in text if char not in string.punctuation])
+    text = re.sub("[0-9]+", '', text)
+
+    return text
+
+
+def sentiment_analysis(tweet):
+    """
+    Returns sentiment analysis data on text
+    """
+    analysis = TextBlob(tweet)
+    print(analysis.sentiment[0])
+    return analysis.sentiment[0]
 
 
 def last_100_tweets(search):
-    #print(api.search(search, count=10, lang='en'))
+    """
+    Returns last 100 tweets on searched item
+    """
     return api.search(search, count=100, lang='en')
 
 
-def setup(text):
-    print(last_100_tweets(text))
-
-    #myStreamListener = MyStreamListener()
-    #myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    #stream = myStream.filter(track=[text])
-
-
-print(setup('nike'))
+if __name__ == '__main__':
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+    stream = myStream.filter(track=['nike'], languages=["en"])
