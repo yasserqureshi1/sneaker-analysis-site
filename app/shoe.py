59c9d495944fe9853c1c5efb7b5d4825ed6b9eed
Yasser Qureshi <yasser.q@hotmail.com>
2020-12-17 23:00:22 +0000

    Fixes

--- app/shoe.py
+++ app/shoe.py
@@ -5,8 +5,10 @@ from app import StockXscraper
 shoe = Blueprint("shoe", __name__)
 
 
-@shoe.route('/<item>')
+@shoe.route('/shoe/<item>')
 def product_item(item):
     data = StockXscraper.getProductDetails(item)
-    return render_template('shoe.html', data=data)
+    labels = [1,2,3,4,5]
+    values = [1,2,3,4,5]
+    return render_template('shoe.html', data=data, labels=labels, values=values)
 
