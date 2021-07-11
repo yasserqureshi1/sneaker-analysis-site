34c2b16f8a9019e3815c805bba6262cc81d22241
Yasser Qureshi <yasser.q@hotmail.com>
2021-07-11 17:46:50 +0100

    Added database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
 from flask import Flask
-from . import main, shoe
+from app.models import db
+from config import Config
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object(Config)
+    db.init_app(app)
 
-    app.register_blueprint(main.home)
-    app.register_blueprint(shoe.shoe)
+    from . import views
+    app.register_blueprint(views.view)
+
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+    app.app_context().push()
+
+    db.session.commit()
+
     return app
